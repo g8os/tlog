61fb372b22f9f61fc971c485d05460592b7c2a64
Iwan Budi Kusnanto <iwanbk@gmail.com>
2017-04-26 18:07:22 +0700

    travis : regenerate capnp in travis build.

--- seastar/install_dep_trusty.sh
+++ seastar/install_dep_trusty.sh
@@ -1,10 +1,13 @@
-# add g++-5 repo for ubuntu trusty which is used by travis
+# this is script to make travis build work.
+# It is because travis use ubuntu trusty and it doesn't have all packages we need.
+# add g++-5 repo
 sudo  apt-get install -y software-properties-common
 sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
 sudo apt-get update
 
 sudo apt-get install -y build-essential  autoconf automake nasm yasm
 
+# install libb2
 cd /tmp
 git clone https://github.com/BLAKE2/libb2.git
 cd libb2
